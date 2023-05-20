914cca7219b1e4329d8e441d63b2c0eb4065e9b4
ximeen <gabriximenes100@gmail.com>
2023-05-20 10:36:28 -0300

    :package: create upload route, and serving statics files

--- source/server.ts
+++ source/server.ts
@@ -2,12 +2,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { statusResponse } from './routes/status'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
+
+app.register(multipart)
 app.register(cors, {
   origin: true,
 })
@@ -16,7 +25,9 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
-app.register(memoriesRoutes, statusResponse)
+app.register(memoriesRoutes)
+app.register(statusResponse)
+app.register(uploadRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server is running in port http://localhost:3333')
